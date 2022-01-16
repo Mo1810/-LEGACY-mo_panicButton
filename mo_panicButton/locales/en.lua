86735f2e28cafa49a8fdf3cc29795a19cdd0a979
Mo1810 <68502762+Mo1810@users.noreply.github.com>
2022-01-16 20:25:49 +0100

    Update en.lua

--- mo_panicButton/locales/en.lua
+++ mo_panicButton/locales/en.lua
@@ -2,6 +2,6 @@ Locales['en'] = {
 	['panicButton_pressed'] = '~r~PanicButton ~s~was pressed. ~b~Officers ~s~were alarmed.',
 	['panicButton_noCop'] = 'You are ~r~no ~b~Officer~s~.',
 	['panicButton_alreadyPressed'] = 'You ~b~recently pressed ~s~the ~r~PanicButton~s~.',
-	['panicButton_dead'] = 'You are ~r~dead~s~ you can't press the ~r~PanicButton ~s~now.',
+	['panicButton_dead'] = 'You are ~r~dead~s~ you can\'t press the ~r~PanicButton ~s~now.',
 	['panicButton_police'] = '~r~WARNING~s~: An ~b~officer ~s~pressed the ~r~PanicButton ~s~. All units there.',
 }
