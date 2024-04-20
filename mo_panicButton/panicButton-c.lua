fd1919b0576968be7febb6da50b4c8d588d67fbd
Mo1810 <68502762+Mo1810@users.noreply.github.com>
2024-04-20 14:17:16 +0200

    Update 1.1.0

--- mo_panicButton/panicButton-c.lua
+++ mo_panicButton/panicButton-c.lua
@@ -3,19 +3,6 @@
 --[[--------------------------]]--
 
 local currentPressed = false
-ESX = nil
-
-Citizen.CreateThread(function()
-	while ESX == nil do
-		TriggerEvent('esx:getSharedObject', function(obj) ESX = obj end)
-		Citizen.Wait(1)
-	end
-	
-	while ESX.GetPlayerData().job == nil do
-		Citizen.Wait(10)
-	end
-	ESX.PlayerData = ESX.GetPlayerData()
-end)
 
 Citizen.CreateThread(function()
 	while true do
